fb75d4f3f81ab98b97a1080a2a36757877170ab4
FedorovD <FedorovD@gmail.com>
2017-10-09 14:48:06 +0300

    TypeScript

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 function component() {
   var element = document.createElement('div');
-  element.innerHTML = join(['Hello', 'webpack'], ' ');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
 
 document.body.appendChild(component());
-
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(json => {
-    console.log('We retrieved some data! AND we\'re confident it will work on a variety of browser distributions.')
-    console.log(json)
-  })
-  .catch(error => console.error('Something went wrong when fetching this data: ', error))
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,22 @@
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
-         entry: {
-               polyfills: './src/polyfills.js',
-               index: './src/index.js'
-             },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [{
-                test: require.resolve('./src/index.js'),
-                use: 'imports-loader?this=>window'
-            },
-            {
-                test: require.resolve('./src/globals.js'),
-                use: 'exports-loader?file,parse=helpers.parse'
-            }
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            join: ['lodash', 'join']
-        })
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
     ]
+  },
+  resolve: {
+    extensions: [ ".tsx", ".ts", ".js" ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
